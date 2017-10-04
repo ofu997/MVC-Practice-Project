45c7e2d2670ed974b1f3a7d8469531aded2eff13
Student <Student@STUDENT-PC>
2017-10-04 16:18:46 -0700

    Add project files.

---
+++ VideoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoApp.Startup))]
+namespace VideoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
