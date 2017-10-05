a3aa5b0a5878b9820534e5e66576fea27df4092d
oliver fu <ofu997@gmail.com>
2017-10-04 18:05:13 -0700

    add project and its ability to show a name for url /Random

---
+++ VideoApp/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//2
+using VideoApp.Models;
+
+namespace VideoApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            // 1
+            var movie = new Movie() { Name = "Psycho" };
+
+            return View(movie);
+        }
+    }
+}
---
+++ VideoApp/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    // to get to url /movies/random we make a controller
+
+}
